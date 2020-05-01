e4ec4ed7d7e4bf1865bff86ade689534f9a0ebad
jpoist97 <jpoist97@gmail.com>
2020-04-30 18:11:05 -0700

    Basic skeleton of website

---
+++ justin-webpage/src/components/Landing.vue
@@ -0,0 +1,44 @@
+<template>
+  
+    <div id="vanta-background">
+
+        <div class="message" data-aos="fade">
+            <div class="text">Hey, I'm Justin Poist.</div>
+            <div class="text">A Software Engineering Student at Cal Poly SLO!</div>
+        </div>
+
+    </div>
+
+
+</template>
+
+<script>
+import Vanta from 'vanta/dist/vanta.net.min';
+import * as THREE from 'three';
+export default {
+mounted() {
+  this.vantaEffect = Vanta({
+    el: "#vanta-background",
+    mouseControls: false,
+    touchControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x333333,
+    backgroundColor: 0x23153c,
+    THREE
+  });
+}
+}
+</script>
+
+<style scoped>
+.message{
+    width: fit-content;
+    height: fit-content;
+    margin: 40vh auto;
+    color: #eeeeee;
+    font-size: 36px;
+}
+</style>
---
+++ justin-webpage/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <landing class="landing"/>
+    <about class="under" data-aos="fade"/>
+    <projects data-aos="fade"/>
+    <contact data-aos="fade"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Landing from '../components/Landing.vue'
+import About from '../components/About.vue'
+import Projects from '../components/Projects.vue'
+import Contact from '../components/Contact.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    landing: Landing,
+    about: About,
+    projects: Projects,
+    contact: Contact
+  }
+}
+</script>
+
+<style scoped>
+.landing{
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 100vw;
+    padding: 15px;
+}
+
+.under{
+  margin-top: 115vh;
+}
+
+*{
+  height: 100vh;
+}
+
+</style>
